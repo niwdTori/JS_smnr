7e15d4b521e72e1c52f839600489baa831e36ef5
Tori <mes.viktoria@mail.ru>
2023-02-28 13:46:45 +0500

    lesson_10

---
+++ Семинар 10/data.js
@@ -0,0 +1,42 @@
+//Задание 2
+const dataInfo = `
+    [
+        {
+            "id": 63372,
+            "answer": "Dartboard",
+            "question": "Its center should be 5 feet, 8 inches off the tavern floor & you can stand 7 feet, 9 1/4 inches away from it",
+            "value": 400,
+            "airdate": "1998-10-20T19:00:00.000Z",
+            "created_at": "2022-12-30T19:05:04.348Z",
+            "updated_at": "2022-12-30T19:05:04.348Z",
+            "category_id": 5627,
+            "game_id": 845,
+            "invalid_count": null,
+            "category": {
+                "id": 5627,
+                "title": "target sports",
+                "created_at": "2022-12-30T19:05:04.138Z",
+                "updated_at": "2022-12-30T19:05:04.138Z",
+                "clues_count": 5
+            }
+        },
+        {
+            "id": 3913,
+            "answer": "<i>Bat Masterson</i>",
+            "question": "1958-61 series starring Gene Barry as a dapper lawman",
+            "value": 500,
+            "airdate": "1985-10-24T19:00:00.000Z",
+            "created_at": "2022-12-30T18:39:11.596Z",
+            "updated_at": "2022-12-30T18:39:11.596Z",
+            "category_id": 588,
+            "game_id": 6609,
+            "invalid_count": null,
+            "category": {
+                "id": 588,
+                "title": "toy names",
+                "created_at": "2022-12-30T18:39:11.491Z",
+                "updated_at": "2022-12-30T18:39:11.491Z",
+                "clues_count": 5
+            }
+        }
+    ]`
