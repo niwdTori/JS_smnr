f011c94250812e0ad9b0cd38c25a78def5459185
Tori <mes.viktoria@mail.ru>
2023-03-03 15:54:45 +0500

    lesson_11

---
+++ Семинар 11/data.js
@@ -0,0 +1,20 @@
+const dataProducts = `
+    [
+        {   
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "img": "img/Rectangle 15 copy.svg",
+            "price": "300",
+            "color": "Red",
+            "size": "XL",
+            "quantity": 2
+        },
+        {
+            "name": "ELLERY X M'O CAPSULE",
+            "img": "img/Rectangle 15 copy (1).svg",
+            "price": "52",
+            "color": "Black",
+            "size": "XL",
+            "quantity": 1
+        }
+    ]
+`
